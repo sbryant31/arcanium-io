717b0252ac57ea941ef7b7662f84675c2a1e0ee9
nick bryant <nickbryant@nicks-MacBook-Pro.local>
2019-09-05 10:58:02 -0700

    team lead packet

--- build-reseller-packet.sh
+++ build-reseller-packet.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 zip arcanium-io-resellers-packet.zip portfolio.pdf offerings.pdf resellers-guide.pdf
+(cd team-lead-packet && zip arcanium-io-team-lead-packet.zip team-lead-program.pdf estimate-template.docx team-lead-guide-guide.pdf)
